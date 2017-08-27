a083e7f69ece5c09f5b1719dbe488d296f7dd138
austburn <austburn@gmail.com>
2017-08-27 10:30:19 -0500

    get api key

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
 import "fmt"
+import "os"
+import "flag"
+
+type Location struct {
+	Lat, Long float32
+	Accuracy int
+}
 
 func main()  {
-	fmt.Println("Hello, world!")
+	var googleApiKey string
+	
+	apiKeyEnv := os.Getenv("GOOGLE_API_KEY")
+	if apiKeyEnv != "" {
+		googleApiKey = apiKeyEnv
+	}
+
+	apiKeyPtr := flag.String("apikey", "", "Google API Key")
+	flag.Parse()
+	if *apiKeyPtr != "" {
+		googleApiKey = *apiKeyPtr
+	}
+
+	if googleApiKey == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Printf("Here is my api key %s\n", googleApiKey)
 }
